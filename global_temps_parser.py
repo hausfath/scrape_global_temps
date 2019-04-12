699a132c6328297e319629674518ed0565ac80f2
hausfath <hausfath@gmail.com>
2019-04-12 09:57:07 -0700

    Added Copernicus/ECMWF, now support GISTEMPv4
    Also fixed error due to change in GISTEMP data format.

--- global_temps_parser.py
+++ global_temps_parser.py
@@ -1,38 +1,47 @@
+#!/usr/local/bin/python3.7
+
 import pandas as pd
 import os, io, requests
-from os.path import join
+from datetime import datetime
 from functools import reduce
+today = datetime.today()
+
 
 #Set to your working directory
-cwd = os.getcwd()
-
-# Add additional path segments to reach download directory
-# e.g. join(cwd, 'downloads') or join(cwd, '..', 'download_here')
-download_dir = join(cwd)
-
+os.chdir('/Users/hausfath/Desktop/Climate Science/')
 
 #File URLs
-gistemp_file = 'https://data.giss.nasa.gov/gistemp/tabledata_v3/GLB.Ts+dSST.csv'
+
+gistemp_file = 'https://data.giss.nasa.gov/gistemp/tabledata_v4/GLB.Ts+dSST.txt' #v4
+#gistemp_file = 'https://data.giss.nasa.gov/gistemp/tabledata_v3/GLB.Ts+dSST.csv' #v3
 noaa_file = 'https://www.ncdc.noaa.gov/cag/global/time-series/globe/land_ocean/p12/12/1880-2050.csv'
 hadley_file = 'https://www.metoffice.gov.uk/hadobs/hadcrut4/data/current/time_series/HadCRUT.4.6.0.0.monthly_ns_avg.txt'
 berkeley_file = 'http://berkeleyearth.lbl.gov/auto/Global/Land_and_Ocean_complete.txt'
 cowtan_way_file = 'http://www-users.york.ac.uk/~kdc3/papers/coverage2013/had4_krig_v2_0_0.txt'
 
+cur_month = str(today.month).rjust(2, '0')
+cur_year = str(today.year)
+copernicus_file = 'https://climate.copernicus.eu/sites/default/files/'+cur_year+'-'+cur_month+'/ts_1month_anomaly_Global_ei_2T_'
+
 #Common baseline period (inclusive)
-start_year = 1971
-end_year = 2000
+rebaseline = True
+start_year = 1979
+end_year = 2016
+
 
 def import_gistemp(filename):
     '''
     Import NASA's GISTEMP, reformatting it to long
     '''
-    urlData = requests.get(filename).content
-    df = pd.read_csv(io.StringIO(urlData.decode('utf-8')), skiprows=1)
-    df.drop(['J-D', 'D-N', 'DJF', 'MAM', 'JJA', 'SON'], axis=1, inplace=True)
+    urlData = requests.get(gistemp_file).content
+    df = pd.read_csv(io.StringIO(urlData.decode('utf-8')), skiprows=7, delim_whitespace=True)
+    df.drop(['J-D', 'D-N', 'DJF', 'MAM', 'JJA', 'SON', 'Year.1'], axis=1, inplace=True)
     for num in range(1, 13):
         df.columns.values[num] = 'month_'+str(num)
-    df_long = pd.wide_to_long(df, ['month_'], i='Year', j='month').reset_index()
-    df_long.columns = ['year', 'month', 'gistemp']
+    df = df[~df['Year'].isin(['Year', 'Divide', 'Multiply', 'Example', 'change'])]
+    df_long = pd.wide_to_long(df.reset_index(), ['month_'], i='Year', j='month').reset_index()
+    df_long.columns = ['year', 'month', 'index', 'gistemp']
+    df_long.drop(columns='index', inplace=True)
     df_long = df_long.apply(pd.to_numeric, errors='coerce')
     df_long.sort_values(by=['year', 'month'], inplace=True)
     df_long.reset_index(inplace=True, drop=True)
@@ -83,18 +92,58 @@ def import_cowtan_way(filename):
     return df
 
 
-def combined_global_temps(start_year, end_year):
+def import_copernicus():
+    '''
+    Import Copernicus/ECMWF reanalysis surface temperature record.
+    '''  
+    copernicus_filename = find_copernicus_file()
+    df = pd.read_csv(copernicus_filename, header=1)
+    df.columns = ['date', 'copernicus', 'copernicus_europe']
+    df['year'] = df['date'].astype(str).str[:4]
+    df['month'] = df['date'].astype(str).str[4:6]
+    df = df[['year', 'month', 'copernicus']].apply(pd.to_numeric)
+    return df
+
+
+def find_copernicus_file():
+    '''
+    Identify the correct Copernicus file to use based on the current month.
+    Should prevent problems on the first day of the month when the record
+    has yet to be updated.
+    '''
+    try:
+        month = str(today.month).rjust(2, '0')
+        copernicus_filename = copernicus_file+str(today.year)+month+'.csv'
+        pd.read_csv(copernicus_filename, header=1)
+    except:
+        month = str(today.month - 1).rjust(2, '0')
+        copernicus_filename = copernicus_file+str(today.year)+month+'.csv'
+        pd.read_csv(copernicus_filename, header=1)
+    return copernicus_filename
+
+
+def combined_global_temps(start_year, end_year, rebaseline=True):
     '''
     Merge all the files together, rebaselining them all to a common period.
     '''
-    hadley = rebaseline(import_hadley(hadley_file), start_year, end_year)
-    gistemp = rebaseline(import_gistemp(gistemp_file), start_year, end_year)
-    noaa = rebaseline(import_noaa(noaa_file), start_year, end_year)
-    berkeley = rebaseline(import_berkeley(berkeley_file), start_year, end_year)
-    cowtan_and_way = rebaseline(import_cowtan_way(cowtan_way_file), start_year, end_year)
-
-    dfs = [hadley, gistemp, noaa, berkeley, cowtan_and_way]
-    df_final = reduce(lambda left,right: pd.merge(left,right,on=['year', 'month'], how='outer'), dfs)
+    if rebaseline == True:
+        hadley = rebaseline(import_hadley(hadley_file), start_year, end_year)
+        gistemp = rebaseline(import_gistemp(gistemp_file), start_year, end_year)
+        noaa = rebaseline(import_noaa(noaa_file), start_year, end_year)
+        berkeley = rebaseline(import_berkeley(berkeley_file), start_year, end_year)
+        cowtan_and_way = rebaseline(import_cowtan_way(cowtan_way_file), start_year, end_year)
+        copernicus = rebaseline(import_copernicus(), start_year, end_year)
+        dfs = [hadley, gistemp, noaa, berkeley, cowtan_and_way, copernicus]
+        df_final = reduce(lambda left,right: pd.merge(left,right,on=['year', 'month'], how='outer'), dfs)
+    else:
+        hadley = import_hadley(hadley_file)
+        gistemp = import_gistemp(gistemp_file)
+        noaa = import_noaa(noaa_file)
+        berkeley = import_berkeley(berkeley_file)
+        cowtan_and_way = import_cowtan_way(cowtan_way_file)
+        copernicus = import_copernicus()
+        dfs = [hadley, gistemp, noaa, berkeley, cowtan_and_way, copernicus]
+        df_final = reduce(lambda left,right: pd.merge(left,right,on=['year', 'month'], how='outer'), dfs)
     return df_final.round(3)
 
 
@@ -110,8 +159,9 @@ def rebaseline(temps, start_year, end_year):
     return temps
 
 
-combined_temps = combined_global_temps(start_year, end_year)
-path_out = join(download_dir,
-                'combined_temps_base_{}_to_{}.csv'.format(start_year,
-                                                          end_year))
-combined_temps.to_csv(path_out, index=False)
+if rebaseline == True:
+    combined_temps = combined_global_temps(start_year, end_year)
+    combined_temps.to_csv('combined_temps_base_'+str(start_year)+'_to_'+str(end_year)+'.csv')
+else:
+    combined_temps = combined_global_temps(start_year, end_year, rebaseline=False)
+    combined_temps.to_csv('combined_temps_separate_base.csv')
